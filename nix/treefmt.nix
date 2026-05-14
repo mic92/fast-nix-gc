f76ddfb79655bafb6a451fd1e080cb53abb62829
Jörg Thalheim <joerg@thalheim.io>
2026-05-14 08:43:00 +0200

    nix/treefmt: use nixfmt-rs

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -4,7 +4,10 @@
 }:
 treefmt-nix.lib.evalModule pkgs {
   projectRootFile = "flake.lock";
-  programs.nixfmt.enable = true;
+  programs.nixfmt = {
+    enable = true;
+    package = pkgs.nixfmt-rs;
+  };
   programs.rustfmt = {
     enable = true;
     edition = "2024";
